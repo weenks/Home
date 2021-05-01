0c4c97d93b1148bb5fb81fd19792af516aa3e560
xChromaz <xchromaz35@gmail.com>
2021-05-01 17:48:23 -0500

    Some Log In styling

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,8 @@ import { Button } from '@material-ui/core'
 import React from 'react'
 import "./Login.css"
 import { auth, provider } from "./firebase"
+import companyLogo from "./Data/CompanyLogo.png"
+
 
 function Login() {
     const signIn = () => {
@@ -9,10 +11,45 @@ function Login() {
     }
     return (
         <div className='login'>
-            <div className="login_logo">
-                <img src='https://fontmeme.com/images/discord-logo-font.png' alt='discord logo' />
+            <header>
+                <img id="LogoImage" src={companyLogo}/>
+                <div id="headerDiv">              
+                    <nav class="NavBar">
+                        <ul class="NavMenu">
+                            <li><a href="./index">Home</a></li>
+                            <li><a href="./HTMLPages/About">About</a></li>
+                            <li id="NavButton"><a href="https://www.weenks.ml/Home/">Log In</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <div className="ButtonContainer">
+                <Button className="btnLog" onClick={signIn}> Log In </Button>
             </div>
-            <Button className="btnLog" onClick={signIn}> Sign in </Button>
+            <footer>
+                <div class="FooterContainer">
+                    <div class="Footer">
+                        <div class="FooterHeading Footer1">
+                            <h2>About Us</h2>
+                            <a href="./HTMLPages/About">About</a>                       
+                            <a href="./HTMLPages/TermsOfService">Terms Of Service</a>
+                        </div>
+                        <div class="FooterHeading Footer2">
+                            <h2>Contact Us</h2>
+                            <a href="./HTMLPages/Support">Support</a>
+                            <a href="./HTMLPages/Contact.html">Contact</a>
+                        </div>
+                        <div class="FooterHeading Footer3">
+                            <h2>Social Media</h2>
+                            <a href="#">Instagram</a>
+                            <a href="#">Twitter</a>
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+        </footer>
         </div>
     )
 }
